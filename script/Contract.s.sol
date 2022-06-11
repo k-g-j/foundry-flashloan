464cb580488630fd25b09a8c64e1d149ca6c8b74
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-10 22:53:05 -0400

    flashloan contract and test

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-import {Contract} from "src/Contract.sol";
+import {Flashloaner} from "src/Contract.sol";
 
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
-        new Contract();
+        // new Flashloaner();
     }
 }
